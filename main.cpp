9857ccdae158238308590a450a773bf87f6494a7
S-bite <you@example.com>
2021-03-04 23:37:18 +0900

    メイン部分をtest()から呼び出すように

--- main.cpp
+++ main.cpp
@@ -1,23 +1,21 @@
 #include "BigInt.h"
 #include "FFT.h"
 
-int main() {
-  // calculate 1234^10=8187505353567209228244052427776
-  BigInt a, b;
+void test() {
+  BigInt a, b, c;
   InitializeFFT(1024);
-  InitializeBigInt(&a, 100);
-  InitializeBigInt(&b, 100);
-
+  InitializeBigInt(a, 100);
+  InitializeBigInt(b, 100);
+  InitializeBigInt(c, 100);
   a.Size = 1;
-  a.Coef[0] = 1234;
-  b.Size = 1;
-  b.Coef[0] = 1;
-  UpdateBigInt(&a);
-  UpdateBigInt(&b);
-
-  for (int i = 0; i < 10; i++) {
-    MulBigInt(&a, &b, &b);
-    UpdateBigInt(&b);
-  }
-  PrintBigInt(&b);
+  a.Coef[0] = 96236657;
+  UpdateBigInt(a);
+  DumpBigInt(a);
+  Inverse(a, b, c);
+  PrintBigInt(a);
+  PrintBigInt(b);
+  PrintBigInt(c);
+  DumpBigInt(b);
 }
+
+int main() { test(); }
